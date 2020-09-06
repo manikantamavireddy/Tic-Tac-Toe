acd2a6b8b64519f2feb709336c52f5e7000689d8
manikantamavireddy <manikantamavireddy@gmail.com>
2020-09-06 15:08:48 +0530

    Updates

---
+++ precache-manifest.552ee664efadc6a310fe004fa63bbd3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de2badb3e29237fbab31f4fc79b68a5e",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "94491e414f2229e22e71",
+    "url": "/Tic-Tac-Toe/static/css/main.48687efc.chunk.css"
+  },
+  {
+    "revision": "aeef5ea0a2c0bc782283",
+    "url": "/Tic-Tac-Toe/static/js/2.d4acae03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-Tac-Toe/static/js/2.d4acae03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94491e414f2229e22e71",
+    "url": "/Tic-Tac-Toe/static/js/main.d8a4ee29.chunk.js"
+  },
+  {
+    "revision": "06aafeaab223f41b0e90",
+    "url": "/Tic-Tac-Toe/static/js/runtime-main.5e49ebc0.js"
+  }
+]);
